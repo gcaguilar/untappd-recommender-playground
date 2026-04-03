5be9879142672765f0b6c6d22f8ff549c53bf7c4
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-04-03 21:37:25 +0200

    chore: configure astro, tailwind, postcss and typescript

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+      redirectToDefaultLocale: false,
+    },
+  },
+  integrations: [
+    tailwindcss(),
+    react(),
+  ],
+});
